387d0e9518a9289703fae0edbd0a2bbf33d0f6de
sergeisg <sergeisobera@gmail.com>
2022-03-28 16:24:25 +0300

    exercise 4.9 submission

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -17,3 +17,11 @@ test('all notes are returned', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(helper.initialNotes.length)
 })
+
+test('the blog post identifier is named id', async () => {
+    const response = await api.get('/api/blogs')
+    for (let blog of response.body){
+        expect(blog).not.toHaveProperty('_id')
+        expect(blog.id).toBeDefined()
+    }
+})
