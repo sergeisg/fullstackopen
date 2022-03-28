20a5e91bae233f5f9270746a33f060bb444fb442
sergeisg <sergeisobera@gmail.com>
2022-03-28 16:40:46 +0300

    exercise 4.10 submission

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
 const app = require('../app')
+const { application } = require('express')
 const api = supertest(app)
 
 beforeEach(async () => {
@@ -24,4 +25,25 @@ test('the blog post identifier is named id', async () => {
         expect(blog).not.toHaveProperty('_id')
         expect(blog.id).toBeDefined()
     }
+})
+
+test('the POST request successfully creates a new blog post', async () => {
+    const newBlogPost = {
+        title: "mock title",
+        author: "mock author",
+        url: "mock url",
+        likes: 5
+    }
+
+    await api  
+        .post('/api/blogs')
+        .send(newBlogPost)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialNotes.length + 1)
+    
+    const contents = response.body.map(r => r.title)
+    expect(contents).toContain('mock title')
 })
