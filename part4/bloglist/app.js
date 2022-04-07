aa210d6f4bd72ca6c564459da79a85651b71e601
sergeisg <sergeisobera@gmail.com>
2022-04-07 15:55:34 +0300

    exercise 4.18-4.19 submission

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info(`connecting to ${config.MONGODB_URI}`)
 
@@ -22,10 +23,15 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const errorHandler = (error, request, response, next) => {
     if (error.name === 'ValidationError') {
         return response.status(400).json({error: error.message})
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({error: 'invalid token'})
+    } else if (error.name === 'TokenExpiredError') {
+        return response.status(401).json({error: 'token expired'})
     }
 }
 app.use(errorHandler)
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,17 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const blog = require('../models/blog')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const listHelper = require('../utils/list_helper')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')){
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
@@ -27,7 +36,12 @@ blogsRouter.get('/mostblogs', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const user = await User.findOne({})
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if(!decodedToken.id){
+        return response.status(401).json({error: 'token missing or invalid'})
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
