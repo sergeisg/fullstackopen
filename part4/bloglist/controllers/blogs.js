c2f90338acf526266d6003eb977ecf02a19bfd1c
sergeisg <sergeisobera@gmail.com>
2022-04-16 10:26:02 +0300

    improved PUT method

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -58,13 +58,8 @@ blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) =
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  }
+  const blog = request.body
+
   const updatedBlog =
         await Blog.findByIdAndUpdate(request.params.id, blog, { new:true, runValidators: true, context: 'query' })
   response.json(updatedBlog)
