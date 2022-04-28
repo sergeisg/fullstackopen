ecf83e77d3333d7857de180a2744125ae7af2fc9
sergeisg <sergeisobera@gmail.com>
2022-04-28 13:45:36 +0300

    exercise 6.2 submission

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -11,16 +11,43 @@ const App = () => {
       type: 'GOOD'
     })
   }
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  const allValues = () => Object.values(store.getState()).reduce((a,b) => a + b)
+
+  const averageValues = () => allValues() === 0 ? 0 : (store.getState().good - store.getState().bad) / allValues()
+
+  const positivePercentage = () => allValues() === 0 ? 0 : (store.getState().good)*100 / allValues()
+
 
   return (
     <div>
+      <h2>give feedback</h2>
       <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={ok}>ok</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={zero}>reset stats</button>
+      <h2>statistics</h2>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+      <div>all {allValues()}</div>
+      <div>average {averageValues()}</div>
+      <div>positive {positivePercentage()}%</div>
     </div>
   )
 }
